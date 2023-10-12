09a4e7e0507001eef93331e9b8d28e9e971f0624
Robert <rsknapek@gmail.com>
2023-10-12 15:22:36 +0200

    Usunięte komentarze, dodano moduł testowy

--- main.py
+++ main.py
@@ -3,80 +3,43 @@ import yagmail
 
 yag = yagmail.SMTP('knapeksecretsanta@gmail.com', oauth2_file='~/Programowanie/Moje projekty/maszyna-losujaca/client_secret.json')
 
-# people_data = {
-#     'Przemek': {
-#         'email': 'przemyslaw.knapek@gmail.com'
-#     },
-#     'Kasia': {
-#         'email': 'katarzyna.rypien@gmail.com'
-#     },
-#     'Robert': {
-#         'email': 'rsknapek@gmail.com'
-#     },
-#     'Katia': {
-#         'email': 'katsiaryna.knapek@gmail.com'
-#     },
-#     'Olek': {
-#         'email': 'aleksanderknapek@gmail.com'
-#     },
-#     'Marta': {
-#         'email': 'martaolszta1989@gmail.com'
-#     },
-#     'Mama': {
-#         'email': 'knapekbozena@gmail.com'
-#     },
-#     'Tata': {
-#         'email': 'knapekwaldemar@gmail.com'
-#     },
-#     'Justyna': {
-#         'email': 'jusia.odair@gmail.com'
-#     },
-#     'Paweł': {
-#         'email': 'cader.pc@gmail.com'
-#     },
-#     'Kasieńka': {
-#         'email': 'kasiaknapek05@gmail.com'
-#     },
-# }
-
 people_data = {
     'Przemek': {
-        'email': 'rsknapek@gmail.com'
+        'email': 'przemyslaw.knapek@gmail.com'
     },
     'Kasia': {
-        'email': 'rsknapek@gmail.com'
+        'email': 'katarzyna.rypien@gmail.com'
     },
     'Robert': {
         'email': 'rsknapek@gmail.com'
     },
     'Katia': {
-        'email': 'rsknapek@gmail.com'
+        'email': 'katsiaryna.knapek@gmail.com'
     },
     'Olek': {
-        'email': 'rsknapek@gmail.com'
+        'email': 'aleksanderknapek@gmail.com'
     },
     'Marta': {
-        'email': 'rsknapek@gmail.com'
+        'email': 'martaolszta1989@gmail.com'
     },
     'Mama': {
-        'email': 'rsknapek@gmail.com'
+        'email': 'knapekbozena@gmail.com'
     },
     'Tata': {
-        'email': 'rsknapek@gmail.com'
+        'email': 'knapekwaldemar@gmail.com'
     },
     'Justyna': {
-        'email': 'rsknapek@gmail.com'
+        'email': 'jusia.odair@gmail.com'
     },
     'Paweł': {
-        'email': 'rsknapek@gmail.com'
+        'email': 'cader.pc@gmail.com'
     },
     'Kasieńka': {
-        'email': 'rsknapek@gmail.com'
+        'email': 'kasiaknapek05@gmail.com'
     },
 }
 
 people = list(people_data.keys())
-
 
 def random_pairs(seq):
     people_from = seq
@@ -91,23 +54,29 @@ def random_pairs(seq):
     return pairs
 
 pairs = random_pairs(people)
-# print(pairs)
 
 for pair in pairs:
     people_data[pair[0]]['gift_to'] = pair[1]
 
-# for item in people_data.items():
-#     print(item[0], item[1]['gift_to'])
-
-# for item in people_data.items():
-#     print(item)
-
 message = '<h3>Cześć {}!<br><br>Maszyna losująca informuje uprzejmie, że wylosowana przez Ciebie osoba, którą obdarujesz w najbliszą Wigilię Bożego Narodzenia to:</h3><h2>{}</h2>'
 
-for k, v in people_data.items():
-    person = k
-    email = v.get('email')
-    gift_to = v.get('gift_to')
-    yag.send(to=email, subject=f"{person}! Oto ważna wiadomość od Świątecznej Maszyny Losującej", contents=message.format(person, gift_to))
+secure = input('Wprowadź hasło do uruchomienia maszyny:\n')
+
+if secure == 'secretsanta':
+    for k, v in people_data.items():
+        person = k
+        email = v.get('email')
+        gift_to = v.get('gift_to')
+        yag.send(to=email, subject=f'{person}! Oto ważna wiadomość od Świątecznej Maszyny Losującej', contents=message.format(person, gift_to))
+    print('Operacja zakończona sukcesem')
+elif secure == 'test':
+    for k, v in people_data.items():
+        person = k
+        email = 'rsknapek@gmail.com'
+        gift_to = v.get('gift_to')
+        yag.send(to=email, subject='To jest email testowy', contents=message.format(person, gift_to))
+    print('Test wypadł pomyślnie')
+else:
+    raise ValueError("Hasło nieprawidłowe!")
 
 
