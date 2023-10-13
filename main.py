d17b04620b06d30bc0f2fdbd8aa8c3c6ef2f334d
Robert <rsknapek@gmail.com>
2023-10-13 10:33:12 +0200

    Przeniesienie bazy danych do pliku json

--- main.py
+++ main.py
@@ -1,43 +1,12 @@
 import random
 import yagmail
+import json
 
 yag = yagmail.SMTP('knapeksecretsanta@gmail.com', oauth2_file='~/Programowanie/Moje projekty/maszyna-losujaca/client_secret.json')
 
-people_data = {
-    'Przemek': {
-        'email': 'przemyslaw.knapek@gmail.com'
-    },
-    'Kasia': {
-        'email': 'katarzyna.rypien@gmail.com'
-    },
-    'Robert': {
-        'email': 'rsknapek@gmail.com'
-    },
-    'Katia': {
-        'email': 'katsiaryna.knapek@gmail.com'
-    },
-    'Olek': {
-        'email': 'aleksanderknapek@gmail.com'
-    },
-    'Marta': {
-        'email': 'martaolszta1989@gmail.com'
-    },
-    'Mama': {
-        'email': 'knapekbozena@gmail.com'
-    },
-    'Tata': {
-        'email': 'knapekwaldemar@gmail.com'
-    },
-    'Justyna': {
-        'email': 'jusia.odair@gmail.com'
-    },
-    'Paweł': {
-        'email': 'cader.pc@gmail.com'
-    },
-    'Kasieńka': {
-        'email': 'kasiaknapek05@gmail.com'
-    },
-}
+with open('people_data.json') as json_file:
+    people_data = json.load(json_file)
+
 
 people = list(people_data.keys())
 
@@ -68,15 +37,13 @@ if secure == 'secretsanta':
         email = v.get('email')
         gift_to = v.get('gift_to')
         yag.send(to=email, subject=f'{person}! Oto ważna wiadomość od Świątecznej Maszyny Losującej', contents=message.format(person, gift_to))
-    print('Operacja zakończona sukcesem')
+    print('Losowanie zakończone sukcesem')
 elif secure == 'test':
     for k, v in people_data.items():
         person = k
-        email = 'rsknapek@gmail.com'
+        email = v.get('email')
         gift_to = v.get('gift_to')
         yag.send(to=email, subject='To jest email testowy', contents=message.format(person, gift_to))
     print('Test wypadł pomyślnie')
 else:
     raise ValueError("Hasło nieprawidłowe!")
-
-
